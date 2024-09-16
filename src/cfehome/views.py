659031cd48ff8b705c56b18eb72d39dbf57e6b28
Ravshanbek44 <rtadjimatov99@gmail.com>
2024-09-17 01:36:50 +0200

    prapare for deploying railway

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -11,19 +11,33 @@ from visits.models import PageVisit
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path= request.path)
+    try:
+        percent= (page_qs.count()*100)/qs.count()
+    except:
+        percent=0    
+
+
     my_title= "My page"
-    html_ = "home.html"
+    html_templates = "home.html"
     context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
-        "percent": (page_qs.count()*100)/qs.count(),
+        "percent": percent,
         "total_visit_count": qs.count()
     }
     PageVisit.objects.create(path=request.path)
-    return render(request, html_, context)
+    return render(request, html_templates, context)
+
+
+
+
 
 
 
