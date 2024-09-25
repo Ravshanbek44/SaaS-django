0ab67a39874f3ba6a39fb227ae6489f55ad9f586
Ravshanbek44 <rtadjimatov99@gmail.com>
2024-09-25 12:44:07 +0200

    register/login

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import home_view, about_view
+from auth_app.views import login_view, register_view
 
 urlpatterns = [
     path("home/", home_view),
+    path('login/', login_view ),
+    path('register/', register_view),
     path("about/", about_view ),
     path('admin/', admin.site.urls),
 ]
